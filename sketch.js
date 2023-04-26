410dec703932e9c5af8371ead413966881d02ffa
Dystopian Disco <DystopianDisco@users.noreply.github.com>
2023-04-26 11:26:08 -0700

    Processing

--- sketch.js
+++ sketch.js
@@ -1,24 +1,34 @@
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style('z-index', '-1'); // Place the canvas behind other elements
-  fill(0, 0, 0, 0);
-  stroke(200); // Set the wave color to light grey (RGB: 200, 200, 200)
-}
-function draw() {
-  background(255); // Set the background color to white (RGB: 255, 255, 255)
-  push();
-  for (let y = 0; y < 900; y += 10) {
-    beginShape();
-    for (let x = 0; x < 900; x += 4) {
-      vertex(
-        x,
-        y +
-          120 * noise(x / 100, frameCount / 500 + y / 115) +
-          60 * noise(y / 115, frameCount / 151)
-      );
-    }
-    endShape();
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('respon1-container');
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1'); // Place the canvas behind other elements
+    background(255, 87, 51);
+    fill(0, 0, 0, 0);
+    stroke (211, 211, 211); // (97, 178, 237, 255); 
   }
-  pop();
-}
+  
+  function draw() {
+    background(255); // Set the background color to white (RGB: 255, 255, 255)
+    push();
+    for (let y = 0; y < 900; y += 10) {
+      beginShape();
+      for (let x = 0; x < 900; x += 4) {
+        vertex(
+          x,
+          y +
+            120 * noise(x / 100, frameCount / 500 + y / 115) +
+            60 * noise(y / 115, frameCount / 151)
+        );
+      }
+      endShape();
+    }
+    pop();
+  }
+  
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  
+
+  
