60f1c5d6dbbfa5f0a055dfb484937ff3171a69e2
Dystopian Disco <DystopianDisco@users.noreply.github.com>
2023-04-26 11:37:09 -0700

    Z index

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('respon1-container');
     canvas.position(0, 0);
-    canvas.style('z-index', '-1'); // Place the canvas behind other elements
+    canvas.style('z-index', '-1');
     background(255, 87, 51);
     fill(0, 0, 0, 0);
-    stroke (211, 211, 211); // (97, 178, 237, 255); 
+    stroke (211, 211, 211); 
   }
-  
+
   function draw() {
-    background(255); // Set the background color to white (RGB: 255, 255, 255)
+    background(255);
     push();
     for (let y = 0; y < 900; y += 10) {
       beginShape();
@@ -25,7 +25,7 @@ function setup() {
     }
     pop();
   }
-  
+
   function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
   }
